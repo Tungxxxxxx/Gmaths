95ded8b1bb6af3b048a6052393b22533f8458f62
tungx1 <tungphamthanhcdt@gmail.com>
2023-11-08 17:02:05 +0700

    s

--- src/screens/homepage/Grades.js
+++ src/screens/homepage/Grades.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 class Grades extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    render() {
-        return (
-            
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <></>;
+  }
 }
-
+const mapStateToProps = (state) => {
+  return {};
+};
 export default Grades;
